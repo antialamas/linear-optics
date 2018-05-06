aed9e5e61d5893c297979ab34ae3b79d945d2190
Jake <jsmith74@tulane.edu>
2018-05-06 12:23:59 -0500

    update main.cpp

--- main.cpp
+++ main.cpp
@@ -18,8 +18,14 @@
 
 #include "LinearOpticalTransform.h"
 #define PI 3.141592653589793
+#include "omp.h"
+#include <unsupported/Eigen/MatrixFunctions>
+
 
 void printState(Eigen::VectorXcd& vec,Eigen::MatrixXi& basis);
+void setToFullHilbertSpace(const int& subPhotons, const int& subModes,Eigen::MatrixXi& nv);
+void setToRandomBasisStates(Eigen::MatrixXi& basis,int photons,int modes,int basisDim);
+
 
 int main(){
 
@@ -110,6 +116,109 @@ int main(){
 
     return 0;
 
+}
+
+
+void printState(Eigen::VectorXcd& vec,Eigen::MatrixXi& basis){
+
+    for(int i=0;i<vec.size();i++){
+
+        std::cout << vec(i) << " * |" << basis.row(i) << ">\n";
+
+    }
+
+    std::cout << std::endl;
+
+    return;
+
+}
+
+void setToRandomBasisStates(Eigen::MatrixXi& basis,int photons,int modes,int basisDim){
+
+    basis = Eigen::MatrixXi::Zero(basisDim,modes);
+
+    for(int i=0;i<basisDim;i++) for(int j=0;j<modes;j++){
+
+        basis(i,j) = rand() % ( 1 + photons - basis.row(i).sum() );
+
+	if(j==modes-1) basis(i,j) += photons - basis.row(i).sum();
+
+    }
+
+    return;
+
+}
+
+inline double doublefactorial(int x){
+
+    assert(x < 171);
+
+    double total=1.0;
+    if (x>=0){
+        for(int i=x;i>0;i--){
+            total=i*total;
+        }
+    }
+    else{
+        std::cout << "invalid factorial" << std::endl;
+        total=-1;
+    }
+    return total;
+}
+
+inline int g(const int& n,const int& m){
+    if(n==0 && m==0){
+        return 0;
+    }
+    else if(n==0 && m>0){
+        return 1;
+    }
+
+    else{
+        return (int)(doublefactorial(n+m-1)/(doublefactorial(n)*doublefactorial(m-1))+0.5);
+    }
+}
+
+void setToFullHilbertSpace(const int& subPhotons, const int& subModes,Eigen::MatrixXi& nv){
+
+    if(subPhotons==0 && subModes == 0){
+
+        nv.resize(0,0);
+
+        return;
+
+    }
+
+    int markers = subPhotons + subModes - 1;
+    int myints[markers];
+    int i = 0;
+    while(i<subPhotons){
+        myints[i]=1;
+        i++;
+    }
+    while(i<markers){
+        myints[i]=0;
+        i++;
+    }
+    nv = Eigen::MatrixXi::Zero(g(subPhotons,subModes),subModes);
+    i = 0;
+    int j,k = 0;
+    do {
+        j = 0;
+        k = 0;
+        while(k<markers){
+        if(myints[k]==1){
+            nv(i,j)=nv(i,j)+1;
+        }
+        else if(myints[k]==0){
+            j++;
+        }
+
+        k++;
+        }
+        i++;
+    } while ( std::prev_permutation(myints,myints+markers) );
+    return;;
 }
 
 
